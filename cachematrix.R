b9ea8a176a10295b7819c3870b20fcec3fc609d5
PRAKASAM2204 <66504771+PRAKASAM2204@users.noreply.github.com>
2020-06-06 09:17:29 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,14 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        get <- function() x
+        getInverse <- function() {
+                if(length(x)%%sqrt(length(x))==0) { ## make sure matrix is square
+                        inv <<- solve(x)
+                }
+        }
+        list(get = get, getInverse = getInverse)
 
 }
 
@@ -11,5 +19,26 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        
         ## Return a matrix that is the inverse of 'x'
+        if(!is.atomic(x)){     
+                inv <- x$getInverse()
+                if(!is.null(inv)) {
+                        message("getting cached data")
+                        return(inv)
+                }     
+        } else {        
+                message("getting the inverse-- no cached data found")
+                return(makeCacheMatrix(x)$getInverse())
+        } 
+        
 }
+
+
+f <- makeCacheMatrix(matrix(1:9, nrow = 3, ncol = 3))
+print(cacheSolve(f))
+
+
+print(cacheSolve(matrix(1:9, nrow = 3, ncol = 3)))
+
+
